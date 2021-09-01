ea166e104f9d3a78464f3ed790a5ac0bb482b97f
DJ Taylor <dtaylor88@gmail.com>
2021-09-01 09:57:10 -0700

    commit tree a297ab7b87a95f8240bee71dd0d492be952ae92f

--- index.js
+++ index.js
@@ -9,7 +9,71 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
+let createEmployeeRecord = function(array) {
+    let obj = {
+        firstName: `${array[0]}`,
+        familyName: `${array[1]}`,
+        title: `${array[2]}`,
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return obj;
+}
+
+let createEmployeeRecords = function(array) {
+    return array.map(arr => createEmployeeRecord(arr))
+}
+
+
+let createTimeInEvent = function(date) {
+    let timeIn = {
+        type: 'TimeIn',
+        hour: parseInt(date.split(' ')[1]),
+        date: date.split(' ')[0]
+    }
+    this.timeInEvents.push(timeIn);
+    return this;
+}
+
+let createTimeOutEvent = function(date) {
+    let timeOut = {
+        type: 'TimeOut',
+        hour: parseInt(date.split(' ')[1]),
+        date: date.split(' ')[0]
+    }
+    this.timeOutEvents.push(timeOut);
+    return this;
+}
+
+let hoursWorkedOnDate = function(date) {
+    let e = new TypeError('Error - no matching timeOut event found for this record.')
+    let inDate = this.timeInEvents.find(function(e) {
+        return e.date === date;
+    })
+
+    let outDate = this.timeOutEvents.find(function(e) {
+        return e.date === date;
+    })
+    
+    if (!outDate) {
+        return e.message;
+    } else {
+        return (outDate.hour - inDate.hour) / 100;
+    }
+}
+
+let wagesEarnedOnDate = function(date) {
+    let payRate = this.payPerHour;
+    let hours = hoursWorkedOnDate.call(this, date);
+    return payRate * hours;
+}
+
+
+
+
+
+ let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,3 +84,17 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let calculatePayroll = function(array) {
+    return array.reduce(function(memo, rec) {
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(array, name) {
+    let result = array.find(function(e) {
+        return e.firstName === name; 
+    })
+    return result;
+}
+
